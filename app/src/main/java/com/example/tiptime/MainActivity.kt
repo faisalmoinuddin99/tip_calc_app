915a44650c97f69dc756ccf4551a091b4d54b1fb
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-02-01 11:41:29 +0530

    Added Keyboard option as Number and singleLine true

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,6 +13,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,7 +53,10 @@ fun EditNumberField() {
     var amountInput by remember {
         mutableStateOf("")
     }
-    TextField(value = amountInput, onValueChange = { amountInput = it })
+    TextField(value = amountInput, onValueChange = { amountInput = it },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        singleLine = true
+    )
 }
 
 @Preview(showBackground = true)
