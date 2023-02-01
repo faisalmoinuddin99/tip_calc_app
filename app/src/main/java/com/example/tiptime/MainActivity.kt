cd62dc2185c9d2c5736a7ef769a6d27973a1ef15
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-02-01 11:10:32 +0530

    Done with adding caluclate tip txt on screen

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.tiptime
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tiptime.ui.theme.TipTimeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +27,22 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-
+                    TipTimeScreen()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun TipTimeScreen() {
+    Column(
+        modifier = Modifier.padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(text = stringResource(id = R.string.calculate_tip), fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
     }
 }
 
@@ -32,6 +51,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     TipTimeTheme {
-        Greeting("Android")
+        TipTimeScreen()
     }
 }
