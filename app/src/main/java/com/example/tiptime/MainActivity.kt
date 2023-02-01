c5b129c50d4c5a8afbebf7f57a14730443e93d42
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-02-01 12:11:02 +0530

    Done with State hositing pattern

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -13,11 +13,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tiptime.ui.theme.TipTimeTheme
+import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,6 +38,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeScreen() {
+
+    // Achieving Re composable
+    var amountInput by remember {
+        mutableStateOf("")
+    }
+    // convert amountInput which is of type string to doube
+    val amount: Double = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
+
     Column(
         modifier = Modifier.padding(32.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
@@ -44,24 +55,37 @@ fun TipTimeScreen() {
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         Spacer(modifier = Modifier.height(16.dp))
-        EditNumberField()
+
+        EditNumberField(value = amountInput, onValueChange = { amountInput = it })
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(text = stringResource(id = R.string.tip_amount, tip),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
 @Composable
-fun EditNumberField() {
-    var amountInput by remember {
-        mutableStateOf("")
-    }
-    TextField(value = amountInput, onValueChange = { amountInput = it },
+fun EditNumberField(value: String, onValueChange: (String) -> Unit) {
+
+    TextField(value = value, onValueChange = onValueChange,
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         label = {
-                Text(text = stringResource(id = R.string.bill_amount),
+            Text(text = stringResource(id = R.string.bill_amount),
                 modifier = Modifier.fillMaxSize()
-                    )
+            )
         },
         singleLine = true
     )
+}
+
+private fun calculateTip(amount: Double, tipPercentage: Double = 15.0): String {
+    var tip = tipPercentage / 100 * amount
+
+    return NumberFormat.getCurrencyInstance().format(tip)
 }
 
 @Preview(showBackground = true)
@@ -73,5 +97,5 @@ fun DefaultPreview() {
 }
 
 /*
-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
+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
  */
